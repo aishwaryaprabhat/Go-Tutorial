83b76e2f5796d7ad8606ea194bf4025e53d4377b
Aishwarya Prabhat <aish.prabhat@shopee.com>
2020-08-14 12:59:08 +0800

    structception

--- structs/main.go
+++ structs/main.go
@@ -5,15 +5,25 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode string
 }
 
 func main() {
-	var alex person
-	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
-	fmt.Println("")
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: "120514",
+		},
+	}
+
+	fmt.Printf("%+v", jim)
+
 }
