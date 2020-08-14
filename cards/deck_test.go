651782dcaf3d454ff6bf5c20a2114a93b4d2703c
Aishwarya Prabhat <aish.prabhat@shopee.com>
2020-08-14 11:54:26 +0800

    simple ptest

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,12 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
+	// Check if length of new deck is correct
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 
+	// Check the last card is 4 of clubs
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be Four of Clubs but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+
+	// loadedDeck
 }
