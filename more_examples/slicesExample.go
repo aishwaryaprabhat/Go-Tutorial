b4d924ef9b3826ce0827dda9c040733acb80b9db
Aishwarya Prabhat <aish.prabhat@shopee.com>
2021-04-09 15:00:07 +0800

    slices

--- more_examples/slicesExample.go
+++ more_examples/slicesExample.go
@@ -5,8 +5,18 @@ import "fmt"
 func main() {
 	//slices solve problems with arrays like fixed size
 	var x [5]int = [5]int{1, 2, 3, 4, 5}
-	var s []int = x[:]
+	var s []int = x[1:3]
 
-	fmt.Println(s)
+	fmt.Println(cap(s))
+
+	//add elements to slice
+	var a []int = []int{1, 2, 3, 4}
+	//adds and returns a new slice
+	b := append(a, 10)
+	fmt.Println(a, b)
+
+	//using make()
+	c := make([]int, 5)
+	fmt.Printf("%T \n", c)
 
 }
