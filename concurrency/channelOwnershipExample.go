f315c6cf0ba3e9656ddd0ab2b5fb9e89765f6498
Aishwarya Prabhat <aish.prabhat@shopee.com>
2021-04-14 23:35:10 +0800

    more channels

---
+++ concurrency/channelOwnershipExample.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	owner := func() <-chan int {
+		ch := make(chan int)
+
+		go func() {
+			defer close(ch)
+			for i := 0; i < 5; i++ {
+				ch <- i
+			}
+		}()
+
+		return ch
+	}
+
+	consumer := func(ch <-chan int) {
+		for v := range ch {
+			fmt.Printf("Received %v\n", v)
+		}
+	}
+
+	ch1 := owner()
+	consumer(ch1)
+
+}
