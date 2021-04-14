fbf197ed4d4bd0103dbe89c7d11f5ae27961fecf
Aishwarya Prabhat <aish.prabhat@shopee.com>
2021-04-14 23:47:03 +0800

    timeout on select

---
+++ concurrency/nonBlockingCommunicationExample.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan string)
+	// ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "ch1"
+	}()
+
+	// go func() {
+	// 	time.Sleep(3 * time.Second)
+	// 	ch2 <- "ch2"
+	// }()
+
+	select {
+	case m1 := <-ch1:
+		fmt.Println(m1)
+	case <-time.After(2 * time.Second):
+		fmt.Println("timeout")
+	}
+
+}
